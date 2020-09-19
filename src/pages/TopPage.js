937ffc0f4223bad9537c2657704ef6bb70d04f28
はつき みなみ <hatsukiminami@hatsuki-no-MacBook-Air.local>
2020-09-19 23:05:59 +0900

    LEVEL.7

--- src/pages/TopPage.js
+++ src/pages/TopPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './pages.css';
 // import reducer from '../reducers/index'
 // import { CLICK_EVENT } from '../actions/index';
@@ -7,8 +7,18 @@ function TopPage() {
   // const [state, dispatch] = useReducer(reducer, []);
   const [user, setUser] = useState('');
   const [cpu, setCpu] = useState('');
+  const [result, setResult] = useState('判定中');
+  const [count, setCount] = useState(0);
 
-  const random = () => {
+  useEffect(
+    () => {
+      judge()
+    },
+    [count]
+  );
+
+
+  const cpuHand = () => {
     let nam = Math.floor(Math.random() * 3)
     if (nam === 0) {
       setCpu('ぐー')
@@ -17,21 +27,42 @@ function TopPage() {
     } else if (nam === 2) {
       setCpu('ぱー')
     }
-  }
+    setCount(prev => prev + 1)
+  };
 
   const rock = () => {
     setUser('ぐー')
-    setCpu(random)
+    cpuHand()
   };
 
   const scissors = () => {
     setUser('ちょき')
-    setCpu(random)
+    cpuHand()
   };
 
   const paper = () => {
     setUser('ぱー')
-    setCpu(random)
+    cpuHand()
+  };
+
+  const judge = () => {
+    if (user !== '') {
+      if (user === cpu) {
+        setResult('あいこです。')
+      } else if (user === 'ぐー' && cpu === 'ちょき') {
+        setResult('あなたの勝ちです！')
+      } else if (user === 'ぐー' && cpu === 'ぱー') {
+        setResult('あなたの負けです！')
+      } else if (user === 'ちょき' && cpu === 'ぱー') {
+        setResult('あなたの勝ちです！')
+      } else if (user === 'ちょき' && cpu === 'ぐー') {
+        setResult('あなたの負けです！')
+      } else if (user === 'ぱー' && cpu === 'ぐー') {
+        setResult('あなたの勝ちです！')
+      } else if (user === 'ぱー' && cpu === 'ちょき') {
+        setResult('あなたの負けです！')
+      }
+    }
   };
 
   return (
@@ -41,6 +72,7 @@ function TopPage() {
       <button onClick={paper}>ぱー</button>
       <p>あなたの手:{user}</p>
       <p>cpuの手:{cpu}</p>
+      <p>勝敗:{result}</p>
     </div>
   );
 };
