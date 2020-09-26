abc46c3036a4ee2ccca7498c6d405f03e9768bc4
はつき みなみ <hatsukiminami@hatsuki-no-MacBook-Air.local>
2020-09-26 15:44:42 +0900

    LEVEL.10

--- src/pages/SecondPage.js
+++ src/pages/SecondPage.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SecondPage = () => {
     return (
         <div>
-
+            <h1>SecondPage</h1>
+            <Link to='/'>TopPageへ移動</Link>
         </div>
-    )
-}
+    );
+};
 
-export default SecondPage
+export default SecondPage;
--- src/pages/TopPage.js
+++ src/pages/TopPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useContext, useState } from 'react';
 // import { fetchGetData } from '../apis/index';
 import Axios from 'axios';
+import { Link } from 'react-router-dom';
 import { Store } from '../store/index';
 import { GET_DATA } from '../actions/index';
 import { Table, Accordion, Button } from 'react-bootstrap';
@@ -24,6 +25,10 @@ const TopPage = () => {
 
   return (
     <div>
+      <div>
+        <h1>TopPage</h1>
+        <Link to='secondpage'>SecondPageへ移動</Link>
+      </div>
       <Table striped bordered hover>
         <thead>
           <tr>
